6eb2faa55bb91e9c6fc4ef6947f8a2f1addb4839
jin-1204-chiu <yee45956978@gmail.com>
2026-05-22 12:41:29 +0800

    fix1 calculate

--- test/test_calculate.py
+++ test/test_calculate.py
@@ -1,13 +1,5 @@
-from calculate import add_func, sub_func
+from calculate import add_func
 def test_add_func():
     assert add_func(1,2) == 3
     assert add_func(0,0) == 0
     assert add_func(-1,-2) == -3
-
-
-    
-def test_sub_func():
-    assert sub_func(2,1) == 1
-    assert sub_func(0,0) == 0
-    assert sub_func(-2,-1) == -1
-    
